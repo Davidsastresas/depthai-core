644e64a5cf0af41d8f689df8b9055f53872fdd26
SzabolcsGergely <szabi@luxonis.com>
2022-02-11 05:40:48 +0200

    TMP: update FW with latest develop + confidence map alignment disabled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ff5576cfe06b16b161925c2b68085ad8d197b553")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5bd3feb9a0f4e7e702264fc07214d43640e781d4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
